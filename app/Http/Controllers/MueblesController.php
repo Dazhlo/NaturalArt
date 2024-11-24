57c38625d037f3d5936c6047b53de3656c87edaa
UnWeyQualkiera <2123100472@soy.utj.edu.mx>
2024-11-24 14:15:03 -0600

    Prueba 1

--- app/Http/Controllers/MueblesController.php
+++ app/Http/Controllers/MueblesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\muebles;
 use Illuminate\Http\Request;
-
+//se agregue este comentario para hacer la prueba
 class MueblesController extends Controller
 {
     public function crear(Request $request)
